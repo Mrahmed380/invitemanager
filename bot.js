81b218e8abc7a3f86c5104dfd0feb189e8d1b9a6
Yurst54 <60783921+Yurst54@users.noreply.github.com>
2020-02-07 18:16:38 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,6 +60,15 @@ client.on("message", message => {
 
 })
 
+client.on("ready", () => {
+console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setActivity(`${prefix}yardım | ${client.users.size} kullanıcıya hizmet veriyor`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+})
+
 client.elevation = message => {
   if (!message.guild) return;
   let permlvl = 0;
