8866248ccf08152be1f2fbdb09c3c3ae2be6cdbe
Yurst54 <60783921+Yurst54@users.noreply.github.com>
2020-02-07 18:13:17 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,6 @@ const fs = require("fs");
 const moment = require("moment");
 const Jimp = require("jimp");
 const db = require("quick.db");
-require("./util/eventLoader")(client);
 require("moment-duration-format");
 
 var prefix = ayarlar.prefix;
@@ -40,6 +39,26 @@ fs.readdir("./komutlar/", (err, files) => {
     });
   });
 });
+
+client.on("message", message => {
+ let client = message.client;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
+
+})
 
 client.elevation = message => {
   if (!message.guild) return;
