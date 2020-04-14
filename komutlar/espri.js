cd110411005f0f68e3aa47286bbefc1452dc1ee6
Yurst54 <60783921+Yurst54@users.noreply.github.com>
2020-04-14 18:07:10 +0400

    Create espri.js

---
+++ komutlar/espri.js
@@ -0,0 +1,27 @@
+
+const Discord = require('discord.js');
+
+exports.run = (client, message) => {
+  
+  var request = require('request');
+  request('https://csr-espri.glitch.me/espri', function (error, response, body) {
+    if (error) return console.log('Hata:', error);
+    else if (!error) { 
+      var api = JSON.parse(body);
+      message.channel.send(`${api.espri}`)
+    }
+  })
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['espiri'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'espri',
+  description: 'Bot espri yapar.',
+  usage: 'espri'
+};
