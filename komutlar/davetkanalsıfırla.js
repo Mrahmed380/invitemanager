95ee5653699c880a1f80bcf60f28c2bc95ed8595
Yurst54 <60783921+Yurst54@users.noreply.github.com>
2020-04-14 17:15:23 +0400

    Update davetkanalsıfırla.js

--- komutlar/davetkanalsıfırla.js
+++ komutlar/davetkanalsıfırla.js
@@ -1,28 +1,43 @@
-const Discord = require("discord.js"),
-      db = require("quick.db"),
-      ayarlar = require("../ayarlar.json"),
-      prefix = ayarlar.prefix;
-
+const Discord = require("discord.js");
+const db = require("quick.db");
 module.exports.run = async (bot, message, args) => {
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bunu yapabilmek için gerekli yetkiye sahip değilsiniz!`)
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+  if (!message.member.hasPermission("KICK_MEMBERS")) {
+    const embed = new Discord.RichEmbed()
+      .setDescription("```Ne yazık ki bu komutu kullanmaya yetkin yok.```")
+    .setFooter(bot.user.username, bot.user.avatarURL)
+      .setColor("BLACK");
+
+    message.channel.send(embed);
+    return;
+  }
 
   let kanal = await db.fetch(`davetkanal_${message.guild.id}`)
 
-  if (!kanal) return message.channel.send(`Davet kanalı ayarlı değil!`)
+  if (!kanal) {
+    return message.channel.send(
+      new Discord.RichEmbed()
+        .setDescription("Davet kanalı zaten ayarlanmamış!")
+      .setFooter(bot.user.username, bot.user.avatarURL)
+        .setColor("BLACK")
+    );
+  }
   db.delete(`davetkanal_${message.guild.id}`)
   const embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
+    .setColor("BLACK")
   .setFooter(bot.user.username, bot.user.avatarURL)
-  .setDescription(`Davet kanalı başarıyla sıfırlandı!`);
+    .setDescription(`Davet kanalı başarıyla sıfırlandı!`);
   message.channel.send(embed);
+return
   
 };
 
 module.exports.conf = {
   aliases: ["davetkanalsıfırla"],
-  permLevel: 0,
+  permLevel: 2,
   enabled: true,
-  guildOnly: true
+  guildOnly: true,
+  kategori: "moderasyon"
 };
 
 module.exports.help = {
