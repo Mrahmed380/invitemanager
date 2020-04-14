7bf48f2dca3c5a0a3ae64948832e47f0dfddb8f1
Yurst54 <60783921+Yurst54@users.noreply.github.com>
2020-04-14 17:14:05 +0400

    Update davetekle.js

--- komutlar/davetekle.js
+++ komutlar/davetekle.js
@@ -1,30 +1,50 @@
-const Discord = require("discord.js"),
-      db = require("quick.db"),
-      ayarlar = require("../ayarlar.json"),
-      prefix = ayarlar.prefix;
-
+const Discord = require("discord.js");
+const db = require("quick.db");
 module.exports.run = async (bot, message, args) => {
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+  if (!message.member.hasPermission("KICK_MEMBERS")) {
+    const embed = new Discord.RichEmbed()
+      .setDescription("```Ne yazık ki bu komutu kullanmaya yetkin yok.```")
+    .setFooter(bot.user.username, bot.user.avatarURL)
+      .setColor("BLACK");
 
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bunu yapabilmek için gerekli yetkiye sahip değilsiniz!`)
+    message.channel.send(embed);
+    return;
+  }
 
-  let kullanici = message.mentions.users.first();
-  let sayi = args.slice(1).join(" ")
-  if (!kullanici) return message.channel.send(`Bir kullanıcıyı etiketlemelisiniz!`)
-  if (!sayi) return message.channel.send(`Bir miktar girmelisiniz!`)
+  let u = message.mentions.users.first();
+let m = args.slice(1).join(" ")
+  if (!u) {
+    return message.channel.send(
+      new Discord.RichEmbed()
+      .setFooter(bot.user.username, bot.user.avatarURL)
+        .setDescription("Lütfen davet eklenecek kişiyi etiketleyiniz!")
+        .setColor("BLACK")
+    );
+  }
+    if (!m) {
+    return message.channel.send(
+      new Discord.RichEmbed()
+      .setFooter(bot.user.username, bot.user.avatarURL)
+        .setDescription("Lütfen eklenecek davet sayısını giriniz.")
+        .setColor("BLACK")
+    );
+  }
   const embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
+    .setColor("BLACK")
   .setFooter(bot.user.username, bot.user.avatarURL)
-  .setDescription(`${kullanici} kullanıcısına ${sayi} adet davet eklendi!`);
+    .setDescription(`${u} Adlı şahsa; ${m} davet eklendi!`);
   message.channel.send(embed);
 
-  db.add(`davet_${kullanici.id}_${message.guild.id}`, +sayi);
+  db.add(`davet_${message.author.id}_${message.guild.id}`, +m);
 };
 
 module.exports.conf = {
   aliases: ["davetekle"],
-  permLevel: 0,
+  permLevel: 2,
   enabled: true,
-  guildOnly: true
+  guildOnly: true,
+  kategori: "moderasyon"
 };
 
 module.exports.help = {
